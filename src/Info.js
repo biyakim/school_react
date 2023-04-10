35afd6264250b554daa71dacf15483347b7a64e3
biyakim <w2116@e-mirim.hs.kr>
2023-04-10 15:09:01 +0900

    작성 페이지

---
+++ src/Info.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  //Info 컴포넌트가 마운트 될 때 useEffect가 2번 호출
+  //useEffect()함수의 두번째 인자값이 배열([])을 추가하면
+  //컴포넌트가 마운트 될 때만 실행한다
+  useEffect(() => {
+    console.log("마운트 될 때만 실행합니다.");
+  }, []);
+  const onChangeName = (e) => {
+    setName(e.target.value);
+    //name값을 변경한다.
+  };
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+    //nickname값을 변경한다.
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <b>이름:</b>
+        {name}
+      </div>
+      <div>
+        <b>닉네임:</b>
+        {nickname}
+      </div>
+    </div>
+  );
+};
+
+export default Info;
